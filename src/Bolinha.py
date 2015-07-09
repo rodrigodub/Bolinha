0deb7080b00a5fcf567e168f7e9dee27ea031ee1
rodrigodub <nobrega@gmail.com>
2015-07-09 15:09:01 +0800

    Started dev environment
    # v0.1.001
    # for Issue #1

---
+++ src/Bolinha.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python3
+
+#################################################
+#                   Bolinha
+# Bolinha is a script to process LAS files
+#
+# v0.1.001
+# for Issue #1
+#
+# Rodrigo Nobrega
+# 20150709-
+#################################################
+__author__ = 'Rodrigo Nobrega'
+
+# import modules
+# import os
+# import datetime
+
+
+# PkGetDirectory()
+class Rodrigo1(object):
+    """
+    Class to do something
+    """
+    def __init__(self):
+        pass
+
+
+# test loop
+def test():
+    print('------------------------')
+    print('Test.')
+    print('------------------------')
+
+
+# main loop
+def main():
+    print('------------------------')
+    print('Main.')
+    print('------------------------')
+
+
+# main, calling main loop
+if __name__ == '__main__':
+    test()
+    # main()
