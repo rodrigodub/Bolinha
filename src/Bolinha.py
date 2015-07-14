b5244f0712e23d9c80954cb5507edce12f5ec9fc
Rodrigo <nobrega@gmail.com>
2015-07-14 23:04:08 +0800

    setTopsFields( ) method looking good.
    # v0.1.011
    # for Issues #11
    The logic seems to be solved. Still needs some string cleaning.

--- src/Bolinha.py
+++ src/Bolinha.py
@@ -5,8 +5,8 @@
 #                   Bolinha
 # Bolinha is a script to process LAS files
 #
-# v0.1.010
-# for Issues #3 #7
+# v0.1.011
+# for Issues #11
 #
 # Rodrigo Nobrega
 # 20150709-20150714
@@ -22,20 +22,42 @@ from Littlehelper import LHFile
 # Las2csv()
 class Las2csv(object):
     """
-    Class to convert LAS 3.0 file into CSVs
+    Class to convert LAS 3.0 file into CSVs.
+    Attributes:
+    inputFileName : string - path/file name
+    inputFile : list - the file contents, each line an item
+    topsList : list - list of TOPS section on file
+    topsFields : list - list of pairs [top section, [field1, field2, ...]]
     """
     def __init__(self):
         self.inputFileName = input('File name? ')
         self.inputFile = self.readFile(self.inputFileName)
-        self.tops_list = self.setTopsList(self.inputFile)
+        self.topsList = self.setTopsList(self.inputFile)
+        self.topsFields = self.setTopsFields(self.inputFile, self.topsList)
 
     def readFile(self, filename):
         """Method to read the input file contents"""
         return LHFile(filename).readInfo()
 
-    def setTopsList(self, inputFile):
+    def setTopsList(self, inputfile):
         """Method to return the list of TOPS sections in the input file"""
-        return [i[:-1] for i in inputFile if '~TOPS_DEFINITION' in i]
+        return [i[:-1] for i in inputfile if '~TOPS_DEFINITION' in i]
+
+    def setTopsFields(self, inputfile, topslist):
+        """Method to define the list of fields for each TOPS section"""
+        result = []
+        for i in topslist:
+            tops = []
+            tops.append(i)
+            idx = inputfile.index(i+'\n')
+            tops.append(inputfile[idx+3])
+            fields = []
+            while inputfile[idx+3] != '#------------------------------------------------------------\n':
+                fields.append(inputfile[idx+3])
+                idx += 1
+            tops.append(fields)
+            result.append(tops)
+        return result
 
 
 # test loop
@@ -61,7 +83,8 @@ def test():
     # , 'v5-AKN1116.las', 'v5-KRC4402_8-4-4.las']
     a = Las2csv()
     # [print(i) for i in a.inputFile]
-    [print(i) for i in a.tops_list]
+    # [print(i) for i in a.topsList]
+    [print(i) for i in a.topsFields]
 
 
 # main loop
